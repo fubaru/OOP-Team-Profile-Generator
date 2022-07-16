ab94003bcdd2168bac0ba8f5365b4bc5b070ae68
fuaus <kotingfunk@gmail.com>
2022-07-16 10:49:02 -0400

    made changes to the describe method test

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -14,9 +14,10 @@ describe("Employee", ()=>{
         it("test all properties",()=>{
         const employee=new Employee('Justin',2,'justin@gmail.com');
 
-        expect(employee.name).toEqual('Justin')
-        expect(employee.id).toEqual(2)
-        expect(employee.email).toEqual('justin@gmail.com')
+        expect(employee.getName()).toEqual('Justin')
+        expect(employee.getId()).toEqual(2)
+        expect(employee.getEmail()).toEqual('justin@gmail.com')
+        expect(employee.getRole()).toEqual('Employee')
         })
         
     });
