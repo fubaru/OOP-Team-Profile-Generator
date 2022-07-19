314e8d007773232d2ab962ab4272983369705a0e
fuaus <kotingfunk@gmail.com>
2022-07-19 14:28:54 -0400

    Manager.test.js: import Manager class. start testing functions using Jest. test all propreties in first describe and test all methods in second describe

---
+++ _tests_/Manager.test.js
@@ -0,0 +1,22 @@
+const Manager = require("../lib/Manager");
+
+describe("Manager", () => {
+    describe("test Manager's properties", () => {
+        it("test all properties", () => {
+            const manager = new Manager('JuJu', 1, 'juju@gmail.com', '123-456-7890');
+
+            expect(manager.name).toEqual('JuJu')
+            expect(manager.id).toEqual(1)
+            expect(manager.email).toEqual('juju@gmail.com')
+            expect(manager.officeNumber).toEqual('123-456-7890')
+        })
+    });
+
+    describe("test Manager's methods", () => {
+        it("test all properties", () => {
+            const manager = new Manager('Mochi', 2, 'Mochi@gmail.com', '465-454-4545');
+          
+            expect(manager.getRole()).toEqual('Manager')
+        })
+    });
+});
